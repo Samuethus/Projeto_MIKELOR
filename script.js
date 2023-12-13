7da413562dc253546f9f1f93e12d6fbbad1141ec
Samuethus <matheusv5703@gmail.com>
2023-12-13 10:46:15 -0300

    AJUSTES

---
+++ script.js
@@ -0,0 +1,17 @@
+let valueDisplay = document.querySelectorAll(".number-numero");
+let interval = 2000 ;
+
+valueDisplay.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute
+    ("data-val"));
+    let duration = Math.floor (interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    },duration);
+});    
+    
